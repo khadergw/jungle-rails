24f27b820f65220bffb01634d12be24519943442
GHadeer Khader <qods_a@yahoo.com>
2022-02-04 17:21:03 +0000

    user-auth

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 3, maximum: 8}
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+end
